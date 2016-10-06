8934a6238d963ef20c71bb13706605db23af56d3
Markus Schlott <schlott@users.fias.science>
2016-10-06 16:47:10 +0200

    Config changes.

--- atlite/config.py
+++ atlite/config.py
@@ -1,10 +1,10 @@
-#Cordex settings
+#Cordex Settings
 name = 'CNRM-CERFACS_CNRM_CM5'
 cutout_dir = '/home/vres/data/markus/cutouts'
 cordex_dir = '/home/vres/data/markus/cordex'
 weather_dataset = 'cordex'
 
-#Projection settings
+#Rotated Pole - Projection Settings
 lon_0 = 180
 o_lon_p = -162
 o_lat_p = 39.25
