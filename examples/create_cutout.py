74afde7bb239bb2f391393974a26565bcb4b652e
Jonas Hörsch <hoersch@fias.uni-frankfurt.de>
2017-04-19 01:28:07 +0200

    examples: create_cutout shows a minimal example

--- examples/create_cutout.py
+++ examples/create_cutout.py
@@ -12,11 +12,12 @@ logging.basicConfig(level=logging.DEBUG)
 
 import atlite
 
-cutout = atlite.Cutout(name="europe-2011-2014",
+cutout = atlite.Cutout(name="europe-2011-01",
                        module="ncep",
                        xs=slice(-12.18798349, 41.56244222),
                        ys=slice(71.65648314, 33.56459975),
-                       years=slice(2011, 2014))
+                       years=slice(2011, 2011),
+                       months=slice(1,1))
 
 #this is where all the work happens - it took 105 minutes on FIAS'
 #beast resi, with 16 cores; the resulting cutout takes 57 GB
