759e6c5ae5961bc4b729ba67de7c27b84ff7c35c
Jonas Hörsch <hoersch@fias.uni-frankfurt.de>
2017-12-22 01:26:22 +0100

    config: Update FIAS paths
    TODO: config should be split out into external config file

--- atlite/config.py
+++ atlite/config.py
@@ -1,5 +1,5 @@
 #Dataset Settings
 cutout_dir = '/home/vres/data/cutouts'
-ncep_dir = '/home/vres/data/rda_ucar'
-cordex_dir = '/home/vres/data/markus/cordex/RCP8.5'
+ncep_dir = '/home/vres-climate/data/rda_ucar'
+cordex_dir = '/home/vres-climate/data/markus/cordex/RCP8.5'
 weather_dataset = {'module': 'cordex', 'model': 'MPI-M-MPI-ESM-LR'}
