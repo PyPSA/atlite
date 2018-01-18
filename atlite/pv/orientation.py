af0097b3fb694248806314f22d649c5e46fb80e8
Jonas Hörsch <hoersch@fias.uni-frankfurt.de>
2018-01-18 17:23:27 +0100

    pv.orientation: Update latitude_optimal orientation

--- atlite/pv/orientation.py
+++ atlite/pv/orientation.py
@@ -17,8 +17,43 @@ def get_orientation(name, **params):
     return getattr(sys.modules[__name__], 'make_{}'.format(name))(**params)
 
 def make_latitude_optimal():
+    """
+    Returns an optimal tilt angle for the given ``lat``, assuming that
+    the panel is facing towards the equator, using a simple method from [1].
+
+    This method only works for latitudes between 0 and 50. For higher
+    latitudes, a static 40 degree angle is returned.
+
+    These results should be used with caution, but there is some
+    evidence that tilt angle may not be that important [2].
+
+    Function and documentation has been adapted from gsee [3].
+
+    [1] http://www.solarpaneltilt.com/#fixed
+    [2] http://dx.doi.org/10.1016/j.solener.2010.12.014
+    [3] https://github.com/renewables-ninja/gsee/blob/master/gsee/pv.py
+
+    Parameters
+    ----------
+    lat : float
+        Latitude in degrees.
+    """
+
     def latitude_optimal(lon, lat, solar_position):
-        return dict(slope=lat, azimuth=0.)
+        if (lat < 0).any():
+            raise NotImplementedError('Not implemented for negative latitudes')
+
+        slope = np.empty_like(lat.values)
+
+        below_25 = lat.values <= np.deg2rad(25)
+        below_50 = lat.values <= np.deg2rad(50)
+
+        slope[below_25] = 0.87 * lat.values[below_25]
+        slope[~below_25 & below_50] = 0.76 * lat.values[~below_25 & below_50] + np.deg2rad(0.31)
+        slope[~below_50] = np.deg2rad(40.)
+
+        return dict(slope=xr.DataArray(slope, coords=lat.coords), azimuth=0.)
+
     return latitude_optimal
 
 def make_constant(slope, azimuth):
