PyPSA/atlite

commit de98391fd969aff8991c4a891efdb2185bd255b0

datasets: Use generators to allow for lazy xarray computation
prepare functions now yield a generator of datasets, when the generator is
exhausted the file is closed and memory is freed afterwards. The new pattern is
simpler and avoids having to load the dataset fully into memory as a preparation
to using dask, as necessary for SARAH.

Before the data handle is freed the generator has to be exhausted! See receive
context manager in sarah.py.

atlite/datasets/ncep.py
@@ -114,62 +114,68 @@ def convert_clip_lower(ds, variable, a_min, value):
     ds[variable] = ds[variable].where(ds[variable] > a_min).fillna(value)
     return ds
 
-def prepare_wnd10m_ncep(ds, yearmonth, xs, ys):
-    ds = convert_lons_lats_ncep(ds, xs, ys)
-    ds = convert_time_hourly_ncep(ds)
-    ds['wnd10m'] = np.sqrt(ds['VGRD_P0_L103_GGA0']**2 + ds['UGRD_P0_L103_GGA0']**2)
-    ds = ds.drop(['VGRD_P0_L103_GGA0', 'UGRD_P0_L103_GGA0'])
-    return [(yearmonth, ds)]
+def prepare_wnd10m_ncep(fn, yearmonth, xs, ys):
+    with xr.open_dataset(fn, engine=engine) as ds:
+        ds = convert_lons_lats_ncep(ds, xs, ys)
+        ds = convert_time_hourly_ncep(ds)
+        ds['wnd10m'] = np.sqrt(ds['VGRD_P0_L103_GGA0']**2 + ds['UGRD_P0_L103_GGA0']**2)
+        ds = ds.drop(['VGRD_P0_L103_GGA0', 'UGRD_P0_L103_GGA0'])
+        yield yearmonth, ds
 
 def prepare_influx_ncep(ds, yearmonth, xs, ys):
-    ds = convert_lons_lats_ncep(ds, xs, ys)
-    ds = convert_unaverage_ncep(ds)
-    ds = convert_time_hourly_ncep(ds)
+    with xr.open_dataset(fn, engine=engine) as ds:
+        ds = convert_lons_lats_ncep(ds, xs, ys)
+        ds = convert_unaverage_ncep(ds)
+        ds = convert_time_hourly_ncep(ds)
 
-    ds = ds.rename({'DSWRF_P8_L1_GGA0': 'influx'})
-    # clipping random fluctuations around zero
-    ds = convert_clip_lower(ds, 'influx', a_min=0.1, value=0.)
-    return [(yearmonth, ds)]
+        ds = ds.rename({'DSWRF_P8_L1_GGA0': 'influx'})
+        # clipping random fluctuations around zero
+        ds = convert_clip_lower(ds, 'influx', a_min=0.1, value=0.)
+        yield yearmonth, ds
 
-def prepare_outflux_ncep(ds, yearmonth, xs, ys):
-    ds = convert_lons_lats_ncep(ds, xs, ys)
-    ds = convert_unaverage_ncep(ds)
-    ds = convert_time_hourly_ncep(ds)
+def prepare_outflux_ncep(fn, yearmonth, xs, ys):
+    with xr.open_dataset(fn, engine=engine) as ds:
+        ds = convert_lons_lats_ncep(ds, xs, ys)
+        ds = convert_unaverage_ncep(ds)
+        ds = convert_time_hourly_ncep(ds)
 
-    ds = ds.rename({'USWRF_P8_L1_GGA0': 'outflux'})
-    # clipping random fluctuations around zero
-    ds = convert_clip_lower(ds, 'outflux', a_min=3., value=0.)
-    return [(yearmonth, ds)]
+        ds = ds.rename({'USWRF_P8_L1_GGA0': 'outflux'})
+        # clipping random fluctuations around zero
+        ds = convert_clip_lower(ds, 'outflux', a_min=3., value=0.)
+        yield yearmonth, ds
 
-def prepare_temperature_ncep(ds, yearmonth, xs, ys):
-    ds = convert_lons_lats_ncep(ds, xs, ys)
-    ds = convert_time_hourly_ncep(ds)
+def prepare_temperature_ncep(fn, yearmonth, xs, ys):
+    with xr.open_dataset(fn, engine=engine) as ds:
+        ds = convert_lons_lats_ncep(ds, xs, ys)
+        ds = convert_time_hourly_ncep(ds)
 
-    ds = ds.rename({'TMP_P0_L103_GGA0': 'temperature'})
-    return [(yearmonth, ds)]
+        ds = ds.rename({'TMP_P0_L103_GGA0': 'temperature'})
+        yield yearmonth, ds
 
-def prepare_runoff_ncep(ds, yearmonth, xs, ys):
-    ds = convert_lons_lats_ncep(ds, xs, ys)
-    # runoff has missing values: set nans to 0
-    ds = ds.fillna(0.)
-    ds = convert_unaccumulate_ncep(ds)
-    ds = convert_time_hourly_ncep(ds)
+def prepare_runoff_ncep(fn, yearmonth, xs, ys):
+    with xr.open_dataset(fn, engine=engine) as ds:
+        ds = convert_lons_lats_ncep(ds, xs, ys)
+        # runoff has missing values: set nans to 0
+        ds = ds.fillna(0.)
+        ds = convert_unaccumulate_ncep(ds)
+        ds = convert_time_hourly_ncep(ds)
 
-    ds = ds.rename({'WATR_P8_L1_GGA0': 'runoff'})
-    return [(yearmonth, ds)]
+        ds = ds.rename({'WATR_P8_L1_GGA0': 'runoff'})
+        yield yearmonth, ds
 
-def prepare_height_ncep(filename, xs, ys, yearmonths):
-    with xr.open_dataset(filename, engine=engine) as ds:
+def prepare_height_ncep(fn, xs, ys, yearmonths):
+    with xr.open_dataset(fn, engine=engine) as ds:
         ds = convert_lons_lats_ncep(ds, xs, ys)
         ds = ds.rename({'HGT_P0_L105_GGA0': 'height'})
-        ds = ds.load()
-        return [(ym, ds) for ym in yearmonths]
+        for ym in yearmonths:
+            yield ym, ds
 
-def prepare_roughness_ncep(ds, yearmonth, xs, ys):
-    ds = convert_lons_lats_ncep(ds, xs, ys)
-    ds = ds.rename({'SFCR_P8_L1_GGA0': 'roughness'})
-    ds = ds.assign_coords(year=yearmonth[0]).assign_coords(month=yearmonth[1])
-    return [(yearmonth, ds)]
+def prepare_roughness_ncep(fn, yearmonth, xs, ys):
+    with xr.open_dataset(fn, engine=engine) as ds:
+        ds = convert_lons_lats_ncep(ds, xs, ys)
+        ds = ds.rename({'SFCR_P8_L1_GGA0': 'roughness'})
+        ds = ds.assign_coords(year=yearmonth[0]).assign_coords(month=yearmonth[1])
+        yield yearmonth, ds
 
 def prepare_meta_ncep(xs, ys, year, month, template, height_config, module):
     fn = next(glob.iglob(template.format(year=year, month=month)))

atlite/datasets/cordex.py
@@ -40,7 +40,6 @@ from ..gis import RotProj
 model = 'MPI-M-MPI-ESM-LR'
 projection = RotProj(dict(proj='ob_tran', o_proj='latlong', lon_0=180,
                           o_lon_p=-162, o_lat_p=39.25))
-engine = None
 
 def rename_and_clean_coords(ds):
     ds = ds.rename({'rlon': 'x', 'rlat': 'y'})
@@ -49,35 +48,42 @@ def rename_and_clean_coords(ds):
                  & {'bnds', 'height', 'rotated_pole'})
     return ds
 
-def prepare_data_cordex(ds, year, months, oldname, newname, xs, ys):
-    ds = rename_and_clean_coords(ds)
-    ds = ds.rename({oldname: newname})
-    ds = ds.sel(x=xs, y=ys)
-    if newname in {'influx', 'outflux'}:
-        # shift averaged data to beginning of bin
-        ds = ds.assign_coords(time=pd.to_datetime(ds.coords["time"].values) - pd.Timedelta(hours=1.5))
-    if newname in {'runoff'}:
-        # shift and fill 6hr average data to beginning of 3hr bins
-        t = pd.to_datetime(ds.coords["time"].values)
-        ds = ds.reindex(method='bfill', time=(t - pd.Timedelta(hours=3.)).union(t))
-    return [((year, m), ds.sel(time="{}-{}".format(year, m)))
-            for m in months]
+def prepare_data_cordex(fn, year, months, oldname, newname, xs, ys):
+    with xr.open_dataset(fn) as ds:
+        ds = rename_and_clean_coords(ds)
+        ds = ds.rename({oldname: newname})
+        ds = ds.sel(x=xs, y=ys)
 
-def prepare_static_data_cordex(ds, year, months, oldname, newname, xs, ys):
-    ds = rename_and_clean_coords(ds)
-    ds = ds.rename({oldname: newname})
-    ds = ds.sel(x=xs, y=ys)
-    return [((year, m), ds)
-            for m in months]
+        if newname in {'influx', 'outflux'}:
+            # shift averaged data to beginning of bin
+            ds = ds.assign_coords(time=(pd.to_datetime(ds.coords["time"].values)
+                                        - pd.Timedelta(hours=1.5)))
+        elif newname in {'runoff'}:
+            # shift and fill 6hr average data to beginning of 3hr bins
+            t = pd.to_datetime(ds.coords["time"].values)
+            ds = ds.reindex(method='bfill', time=(t - pd.Timedelta(hours=3.)).union(t))
 
-def prepare_weather_types_cordex(ds, year, months, oldname, newname, xs, ys):
-    ds = ds.rename({oldname: newname})
-    return [((year, m), ds.sel(time="{}-{}".format(year, m)))
-            for m in months]
+        for m in months:
+            yield (year, m), ds.sel(time="{}-{}".format(year, m))
+
+def prepare_static_data_cordex(fn, year, months, oldname, newname, xs, ys):
+    with xr.open_dataset(fn) as ds:
+        ds = rename_and_clean_coords(ds)
+        ds = ds.rename({oldname: newname})
+        ds = ds.sel(x=xs, y=ys)
+
+        for m in months:
+            yield (year, m), ds
+
+def prepare_weather_types_cordex(fn, year, months, oldname, newname, xs, ys):
+    with xr.open_dataset(fn) as ds:
+        ds = ds.rename({oldname: newname})
+        for m in months:
+            yield (year, m), ds.sel(time="{}-{}".format(year, m))
 
 def prepare_meta_cordex(xs, ys, year, month, template, height_config, module, model=model):
     fn = next(glob.iglob(template.format(year=year, model=model)))
-    with xr.open_dataset(fn, engine=engine) as ds:
+    with xr.open_dataset(fn) as ds:
         ds = rename_and_clean_coords(ds)
         ds = ds.coords.to_dataset()
         meta = ds.sel(time="{}-{}".format(year, month),
@@ -109,7 +115,6 @@ def tasks_yearly_cordex(xs, ys, yearmonths, prepare_func, template, oldname, new
     return [dict(prepare_func=prepare_func,
                  xs=xs, ys=ys, oldname=oldname, newname=newname,
                  fn=next(glob.iglob(template.format(year=year, model=model))),
-                 engine=engine,
                  year=year, months=list(map(itemgetter(1), yearmonths)))
             for year, yearmonths in groupby(yearmonths, itemgetter(0))]
 
@@ -143,13 +148,13 @@ weather_data_config = {
                    oldname='mrro', newname='runoff',
                    template=os.path.join(cordex_dir, '{model}', 'runoff', 'mrro_*_{year}*.nc')),
     'height': dict(tasks_func=tasks_yearly_cordex,
-                      prepare_func=prepare_static_data_cordex,
-                      oldname='orog', newname='height',
-                      template=os.path.join(cordex_dir, '{model}', 'altitude', 'orog_*.nc')),
+                   prepare_func=prepare_static_data_cordex,
+                   oldname='orog', newname='height',
+                   template=os.path.join(cordex_dir, '{model}', 'altitude', 'orog_*.nc')),
     'CWT': dict(tasks_func=tasks_yearly_cordex,
-                      prepare_func=prepare_weather_types_cordex,
-                      oldname='CWT', newname='CWT',
-                      template=os.path.join(cordex_dir, '{model}', 'weather_types', 'CWT_*_{year}*.nc')),
+                prepare_func=prepare_weather_types_cordex,
+                oldname='CWT', newname='CWT',
+                template=os.path.join(cordex_dir, '{model}', 'weather_types', 'CWT_*_{year}*.nc')),
 }
 
 meta_data_config = dict(prepare_func=prepare_meta_cordex,

atlite/datasets/era5.py
@@ -29,6 +29,8 @@ import tempfile
 import shutil
 from six.moves import range
 from contextlib import contextmanager
+import logging
+logger = logging.getLogger(__name__)
 
 try:
     from ecmwfapi import ECMWFDataServer
@@ -40,7 +42,7 @@ except ImportError:
 projection = 'latlong'
 
 @contextmanager
-def _get_data(target, **updates):
+def _get_data(target, chunks=None, **updates):
     if not has_ecmwfapi:
         raise RuntimeError(
             "Need installed ecmwfapi python package available from "
@@ -59,7 +61,7 @@ def _get_data(target, **updates):
     request.update(updates)
     server.retrieve(request)
 
-    with xr.open_dataset(target) as ds:
+    with xr.open_dataset(target, chunks=chunks) as ds:
         yield ds
 
     os.unlink(target)
@@ -73,9 +75,10 @@ def _add_height(ds):
     ds = ds.drop('z')
     return ds
 
-def _rename_and_clean_coords(ds):
+def _rename_and_clean_coords(ds, add_lon_lat=True):
     ds = ds.rename({'longitude': 'x', 'latitude': 'y'})
-    ds = ds.assign_coords(lon=ds.coords['x'], lat=ds.coords['y'])
+    if add_lon_lat:
+        ds = ds.assign_coords(lon=ds.coords['x'], lat=ds.coords['y'])
     return ds
 
 def prepare_meta_era5(xs, ys, year, month, module):
@@ -95,7 +98,7 @@ def prepare_meta_era5(xs, ys, year, month, module):
 
         return ds.load()
 
-def prepare_for_sarah(year, month, xs, ys, dx, dy):
+def prepare_for_sarah(year, month, xs, ys, dx, dy, chunks=None):
     tmpdir = tempfile.mkdtemp()
     fns = [os.path.join(tmpdir, '_{}{:02}_{}.nc'.format(year, month, i)) for i in range(2)]
 
@@ -109,28 +112,32 @@ def prepare_for_sarah(year, month, xs, ys, dx, dy):
 
     with _get_data(fns[0], type='an', date=date1, area=area, grid=grid,
                    time='/'.join('{:02}'.format(s) for s in range(0, 24)),
-                   param='167') as ds, \
+                   param='167', chunks=chunks) as ds, \
          _get_data(fns[1], type='fc', date=date2, area=area, grid=grid,
                    time='06:00:00/18:00:00',
                    step='0/1/2/3/4/5/6/7/8/9/10/11',
-                   param='212/169/176') as ds_fc:
-        ds = xr.merge([ds, ds_fc], join='left').load()
-    ds = _rename_and_clean_coords(ds)
+                   param='212/169/176', chunks={}) as ds_fc:
+        ds = xr.merge([ds, ds_fc], join='left')
+        ds = _rename_and_clean_coords(ds, add_lon_lat=False)
 
-    ds = ds.rename({'t2m': 'temperature'})
-    ds = ds.rename({'tisr': 'influx_toa'})
+        ds = ds.rename({'t2m': 'temperature'})
+        ds = ds.rename({'tisr': 'influx_toa'})
 
-    with np.errstate(divide='ignore', invalid='ignore'):
+        logger.debug("Calculate albedo")
         ds['albedo'] = (((ds['ssrd'] - ds['ssr'])/ds['ssrd']).fillna(0.)
                         .assign_attrs(units='(0 - 1)', long_name='Albedo'))
-    ds = ds.drop(['ssrd', 'ssr'])
+        ds = ds.drop(['ssrd', 'ssr'])
 
-    # Convert from energy to power J m**-2 -> W m**-2
-    ds['influx_toa'] /= 60.*60.
-    ds['influx_toa'].attrs['units'] = 'W m**-2'
+        logger.debug("Fixing units of influx_toa")
+        # Convert from energy to power J m**-2 -> W m**-2
+        ds['influx_toa'] /= 60.*60.
+        ds['influx_toa'].attrs['units'] = 'W m**-2'
+
+        logger.debug("Yielding ERA5 results")
+        yield ds.chunk(chunks)
+        logger.debug("Cleaning up ERA5")
 
     shutil.rmtree(tmpdir)
-    return ds
 
 def prepare_month_era5(year, month, xs, ys):
     tmpdir = tempfile.mkdtemp()
@@ -157,45 +164,46 @@ def prepare_month_era5(year, month, xs, ys):
                    param='129/244') as ds_m:
 
         ds_m = ds_m.isel(time=0, drop=True)
-        ds = xr.merge([ds, ds_fc, ds_m], join='left').load()
+        ds = xr.merge([ds, ds_fc, ds_m], join='left')
+
+        ds = _rename_and_clean_coords(ds)
+        ds = _add_height(ds)
+
+        # Help on the radiation quantities is in
+        # https://www.ecmwf.int/sites/default/files/radiation_in_mars.pdf
+        # TISR Top Incident Solar Radiation 212
+        # SSRD Surface Solar Rad Downwards 169
+        # SSR Surface net Solar Radiation 176
+        # FDIR Direct solar radiation at surface 21.228
+        ds = ds.rename({'fdir': 'influx_direct', 'tisr': 'influx_toa'})
+        with np.errstate(divide='ignore', invalid='ignore'):
+            ds['albedo'] = (((ds['ssrd'] - ds['ssr'])/ds['ssrd']).fillna(0.)
+                            .assign_attrs(units='(0 - 1)', long_name='Albedo'))
+        ds['influx_diffuse'] = ((ds['ssrd'] - ds['influx_direct'])
+                                .assign_attrs(units='J m**-2',
+                                            long_name='Surface diffuse solar radiation downwards'))
+        ds = ds.drop(['ssrd', 'ssr'])
+
+        # Convert from energy to power J m**-2 -> W m**-2
+        for a in ('influx_direct', 'influx_diffuse', 'influx_toa'):
+            ds[a] /= 60.*60.
+            ds[a].attrs['units'] = 'W m**-2'
+
+        ds['wnd100m'] = (np.sqrt(ds['u100']**2 + ds['v100']**2)
+                        .assign_attrs(units=ds['u100'].attrs['units'],
+                                    long_name="100 metre wind speed"))
+        ds = ds.drop(['u100', 'v100'])
+
+        ds = ds.rename({'fsr': 'roughness'})
+
+        ds = ds.rename({'ro': 'runoff',
+                        't2m': 'temperature',
+                        'sp': 'pressure'})
+
+        yield (year, month), ds
 
     shutil.rmtree(tmpdir)
 
-    ds = _rename_and_clean_coords(ds)
-    ds = _add_height(ds)
-
-    # Help on the radiation quantities is in
-    # https://www.ecmwf.int/sites/default/files/radiation_in_mars.pdf
-    # TISR Top Incident Solar Radiation 212
-    # SSRD Surface Solar Rad Downwards 169
-    # SSR Surface net Solar Radiation 176
-    # FDIR Direct solar radiation at surface 21.228
-    ds = ds.rename({'fdir': 'influx_direct', 'tisr': 'influx_toa'})
-    with np.errstate(divide='ignore', invalid='ignore'):
-        ds['albedo'] = (((ds['ssrd'] - ds['ssr'])/ds['ssrd']).fillna(0.)
-                        .assign_attrs(units='(0 - 1)', long_name='Albedo'))
-    ds['influx_diffuse'] = ((ds['ssrd'] - ds['influx_direct'])
-                            .assign_attrs(units='J m**-2',
-                                          long_name='Surface diffuse solar radiation downwards'))
-    ds = ds.drop(['ssrd', 'ssr'])
-
-    # Convert from energy to power J m**-2 -> W m**-2
-    for a in ('influx_direct', 'influx_diffuse', 'influx_toa'):
-        ds[a] /= 60.*60.
-        ds[a].attrs['units'] = 'W m**-2'
-
-    ds['wnd100m'] = (np.sqrt(ds['u100']**2 + ds['v100']**2)
-                     .assign_attrs(units=ds['u100'].attrs['units'],
-                                   long_name="100 metre wind speed"))
-    ds = ds.drop(['u100', 'v100'])
-
-    ds = ds.rename({'fsr': 'roughness'})
-
-    ds = ds.rename({'ro': 'runoff',
-                    't2m': 'temperature',
-                    'sp': 'pressure'})
-
-    return [((year, month), ds)]
 
 def tasks_monthly_era5(xs, ys, yearmonths, prepare_func, meta_attrs):
     if not isinstance(xs, slice):

atlite/preparation.py
@@ -43,10 +43,6 @@ def cutout_do_task(task, write_to_file=True):
         datasetfns = task.pop('datasetfns')
 
     try:
-        if 'fn' in task:
-            fn = task.pop('fn')
-            engine = task.pop('engine')
-            task['ds'] = xr.open_dataset(fn, engine=engine)
         data = prepare_func(**task)
         if data is None:
             data = []
@@ -56,20 +52,18 @@ def cutout_do_task(task, write_to_file=True):
                 ## TODO : rewrite using plain netcdf4 to add variables
                 ## to the same file one by one
                 fn = datasetfns[yearmonth]
+                logger.debug("Writing to %s", os.path.basename(fn))
                 ds.to_netcdf(fn)
                 logger.debug("Write variable(s) %s to %s generated by %s",
                              ", ".join(ds.data_vars),
                              os.path.basename(fn),
                              prepare_func.__name__)
         else:
-            return [(ym,ds.load()) for ym, ds in data]
+            return data
     except Exception as e:
         logger.exception("Exception occured in the task with prepare_func `%s`: %s",
                          prepare_func.__name__, e.args[0])
         raise e
-    finally:
-        if 'ds' in task:
-            task['ds'].close()
 
 def cutout_prepare(cutout, overwrite=False, nprocesses=None, gebco_height=False):
     if cutout.prepared and not overwrite:
@@ -80,8 +74,8 @@ def cutout_prepare(cutout, overwrite=False, nprocesses=None, gebco_height=False)
 
     cutout_dir = cutout.cutout_dir
     yearmonths = cutout.coords['year-month'].to_index()
-    xs = cutout.coords['x']
-    ys = cutout.coords['y']
+    xs = cutout.meta.indexes['x']
+    ys = cutout.meta.indexes['y']
 
     if gebco_height:
         logger.info("Interpolating gebco to the dataset grid")
@@ -131,14 +125,17 @@ def cutout_prepare(cutout, overwrite=False, nprocesses=None, gebco_height=False)
     for fn in map(cutout.datasetfn, yearmonths.tolist()):
         base, ext = os.path.splitext(fn)
         fns = glob(base + "-*" + ext)
+        if len(fns) == 1 and not gebco_height:
+            # Fast-path
+            os.rename(fns[0], fn)
+        else:
+            with xr.open_mfdataset(fns) as ds:
+                if gebco_height:
+                    ds['height'] = cutout.meta['height']
 
-        with xr.open_mfdataset(fns) as ds:
-            if gebco_height:
-                ds['height'] = cutout.meta['height']
+                ds.to_netcdf(fn)
 
-            ds.to_netcdf(fn)
-
-        for tfn in fns: os.unlink(tfn)
+            for tfn in fns: os.unlink(tfn)
         logger.debug("Completed file %s", os.path.basename(fn))
 
     logger.info("Cutout '%s' has been successfully prepared", cutout.name)
@@ -160,6 +157,10 @@ def cutout_produce_specific_dataseries(cutout, yearmonth, series_name):
 def cutout_get_meta(cutout, xs, ys, years, months=None, **dataset_params):
     if months is None:
         months = slice(1, 12)
+    if ys.stop > ys.start:
+        logger.warn("ys slices are expected from north to south, i.e. slice(70, 40) for europe.")
+        ys = slice(ys.stop, ys.start, -ys.step if ys.step is not None else None)
+
     meta_kwds = cutout.meta_data_config.copy()
     meta_kwds.update(dataset_params)
 

commit 64a74439a99d9e11e27bb554f369282c2a302ad6

ncep: Fix height map regression introduced by refactoring in de98391

atlite/datasets/ncep.py
@@ -114,7 +114,7 @@ def convert_clip_lower(ds, variable, a_min, value):
     ds[variable] = ds[variable].where(ds[variable] > a_min).fillna(value)
     return ds
 
-def prepare_wnd10m_ncep(fn, yearmonth, xs, ys):
+def prepare_wnd10m_ncep(fn, yearmonth, xs, ys, engine=engine):
     with xr.open_dataset(fn, engine=engine) as ds:
         ds = convert_lons_lats_ncep(ds, xs, ys)
         ds = convert_time_hourly_ncep(ds)
@@ -122,7 +122,7 @@ def prepare_wnd10m_ncep(fn, yearmonth, xs, ys):
         ds = ds.drop(['VGRD_P0_L103_GGA0', 'UGRD_P0_L103_GGA0'])
         yield yearmonth, ds
 
-def prepare_influx_ncep(ds, yearmonth, xs, ys):
+def prepare_influx_ncep(fn, yearmonth, xs, ys, engine=engine):
     with xr.open_dataset(fn, engine=engine) as ds:
         ds = convert_lons_lats_ncep(ds, xs, ys)
         ds = convert_unaverage_ncep(ds)
@@ -133,7 +133,7 @@ def prepare_influx_ncep(ds, yearmonth, xs, ys):
         ds = convert_clip_lower(ds, 'influx', a_min=0.1, value=0.)
         yield yearmonth, ds
 
-def prepare_outflux_ncep(fn, yearmonth, xs, ys):
+def prepare_outflux_ncep(fn, yearmonth, xs, ys, engine=engine):
     with xr.open_dataset(fn, engine=engine) as ds:
         ds = convert_lons_lats_ncep(ds, xs, ys)
         ds = convert_unaverage_ncep(ds)
@@ -144,7 +144,7 @@ def prepare_outflux_ncep(fn, yearmonth, xs, ys):
         ds = convert_clip_lower(ds, 'outflux', a_min=3., value=0.)
         yield yearmonth, ds
 
-def prepare_temperature_ncep(fn, yearmonth, xs, ys):
+def prepare_temperature_ncep(fn, yearmonth, xs, ys, engine=engine):
     with xr.open_dataset(fn, engine=engine) as ds:
         ds = convert_lons_lats_ncep(ds, xs, ys)
         ds = convert_time_hourly_ncep(ds)
@@ -152,7 +152,7 @@ def prepare_temperature_ncep(fn, yearmonth, xs, ys):
         ds = ds.rename({'TMP_P0_L103_GGA0': 'temperature'})
         yield yearmonth, ds
 
-def prepare_soil_temperature_ncep(fn, yearmonth, xs, ys):
+def prepare_soil_temperature_ncep(fn, yearmonth, xs, ys, engine=engine):
     with xr.open_dataset(fn, engine=engine) as ds:
         ds = convert_lons_lats_ncep(ds, xs, ys)
         ds = convert_time_hourly_ncep(ds)
@@ -160,7 +160,7 @@ def prepare_soil_temperature_ncep(fn, yearmonth, xs, ys):
         ds = ds.rename({'TMP_P0_2L106_GGA0': 'soil temperature'})
         yield yearmonth, ds
 
-def prepare_runoff_ncep(fn, yearmonth, xs, ys):
+def prepare_runoff_ncep(fn, yearmonth, xs, ys, engine=engine):
     with xr.open_dataset(fn, engine=engine) as ds:
         ds = convert_lons_lats_ncep(ds, xs, ys)
         # runoff has missing values: set nans to 0
@@ -171,21 +171,21 @@ def prepare_runoff_ncep(fn, yearmonth, xs, ys):
         ds = ds.rename({'WATR_P8_L1_GGA0': 'runoff'})
         yield yearmonth, ds
 
-def prepare_height_ncep(fn, xs, ys, yearmonths):
+def prepare_height_ncep(fn, xs, ys, yearmonths, engine=engine):
     with xr.open_dataset(fn, engine=engine) as ds:
         ds = convert_lons_lats_ncep(ds, xs, ys)
         ds = ds.rename({'HGT_P0_L105_GGA0': 'height'})
         for ym in yearmonths:
             yield ym, ds
 
-def prepare_roughness_ncep(fn, yearmonth, xs, ys):
+def prepare_roughness_ncep(fn, yearmonth, xs, ys, engine=engine):
     with xr.open_dataset(fn, engine=engine) as ds:
         ds = convert_lons_lats_ncep(ds, xs, ys)
         ds = ds.rename({'SFCR_P8_L1_GGA0': 'roughness'})
         ds = ds.assign_coords(year=yearmonth[0]).assign_coords(month=yearmonth[1])
         yield yearmonth, ds
 
-def prepare_meta_ncep(xs, ys, year, month, template, height_config, module):
+def prepare_meta_ncep(xs, ys, year, month, template, height_config, module, engine=engine):
     fn = next(glob.iglob(template.format(year=year, month=month)))
     with xr.open_dataset(fn, engine=engine) as ds:
         ds = ds.coords.to_dataset()
@@ -200,7 +200,7 @@ def prepare_meta_ncep(xs, ys, year, month, template, height_config, module):
     height_tasks_func = height_config.pop('tasks_func')
     height_task, = height_tasks_func(xs, ys, [(year, month)], meta_attrs={}, **height_config)
     height_prepare_func = height_task.pop('prepare_func')
-    _, ds = height_prepare_func(**height_task)[0]
+    _, ds = next(height_prepare_func(**height_task))
 
     meta['height'] = ds['height']
 
@@ -217,7 +217,7 @@ def tasks_monthly_ncep(xs, ys, yearmonths, prepare_func, template, meta_attrs):
 def tasks_height_ncep(xs, ys, yearmonths, prepare_func, template, meta_attrs, **extra_args):
     return [dict(prepare_func=prepare_func,
                  xs=xs, ys=ys, yearmonths=yearmonths,
-                 filename=next(glob.iglob(template)),
+                 fn=next(glob.iglob(template)),
                  **extra_args)]
 
 weather_data_config = {