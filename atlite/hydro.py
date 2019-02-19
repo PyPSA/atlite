a96f77c5aeba67a1df1dab0ee16ff1025f6e10e9
Jonas Hoersch <jonas@chaoflow.net>
2019-02-19 01:12:02 +0100

    hydro: [tiny] Reflow long code lines

--- atlite/hydro.py
+++ atlite/hydro.py
@@ -41,7 +41,8 @@ Basins = namedtuple('Basins', ['plants', 'meta', 'shapes'])
 def find_basin(shapes, lon, lat):
     hids = shapes.index[shapes.intersects(Point(lon, lat))]
     if len(hids) > 1:
-        logger.warning(f"The point ({lon}, {lat}) is in several basins: {hids}. Assuming the first one.")
+        logger.warning(f"The point ({lon}, {lat}) is in several basins: {hids}. "
+                        "Assuming the first one.")
     return hids[0]
 
 def find_upstream_basins(meta, hid):
@@ -61,16 +62,21 @@ def determine_basins(plants, hydrobasins, show_progress=True):
         "but received `type(hydrobasins) = {}`".format(type(hydrobasins))
     )
 
-    missing_columns = pd.Index(['HYBAS_ID', 'DIST_MAIN', 'NEXT_DOWN', 'geometry']).difference(hydrobasins.columns)
-    assert missing_columns.empty, \
-        "Couldn't find the column(s) {} in the hydrobasins dataset.".format(", ".join(missing_columns))
+    missing_columns = (pd.Index(['HYBAS_ID', 'DIST_MAIN', 'NEXT_DOWN', 'geometry'])
+                       .difference(hydrobasins.columns))
+    assert missing_columns.empty, (
+        "Couldn't find the column(s) {} in the hydrobasins dataset."
+        .format(", ".join(missing_columns))
+    )
 
     hydrobasins = hydrobasins.set_index("HYBAS_ID")
 
     meta = hydrobasins[hydrobasins.columns.difference(('geometry',))]
     shapes = hydrobasins['geometry']
 
-    maybe_progressbar = make_optional_progressbar(show_progress, "Determine upstream basins per plant", len(plants))
+    maybe_progressbar = make_optional_progressbar(
+        show_progress, "Determine upstream basins per plant", len(plants)
+    )
 
     plant_basins = []
     for p in maybe_progressbar(plants.itertuples()):
@@ -86,11 +92,14 @@ def shift_and_aggregate_runoff_for_plants(basins, runoff, flowspeed=1, show_prog
                           [('plant', basins.plants.index),
                            ('time' , runoff.coords["time"])])
 
-    maybe_progressbar = make_optional_progressbar(show_progress, "Shift and aggregate runoff by plant", len(basins.plants))
+    maybe_progressbar = make_optional_progressbar(
+        show_progress, "Shift and aggregate runoff by plant", len(basins.plants)
+    )
 
     for ppl in maybe_progressbar(basins.plants.itertuples()):
         inflow_plant = inflow.loc[dict(plant=ppl.Index)]
-        distances = basins.meta.loc[ppl.upstream, "DIST_MAIN"] - basins.meta.at[ppl.hid, "DIST_MAIN"]
+        distances = (basins.meta.loc[ppl.upstream, "DIST_MAIN"] -
+                     basins.meta.at[ppl.hid, "DIST_MAIN"])
         nhours = (distances * (flowspeed * 3.6) + 0.5).astype(int)
 
         for b in ppl.upstream:
