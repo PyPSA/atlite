4f525ef9755616472fa19df426a45d65dc0b89a0
euronion <42553970+euronion@users.noreply.github.com>
2019-07-20 14:58:49 +0200

    Update example create_cutout for new config

--- examples/create_cutout.py
+++ examples/create_cutout.py
@@ -1,24 +1,27 @@
+# In this example we assume you have
+# * Installed the Copernicus Climate Data Store 'cdsapi' package
+# * Registered and setup your cdsapi key as described here https://cds.climate.copernicus.eu/api-how-to
+# 
+# while atlite does support other data sources, using ERA5 from the CDS
+# is good for a quick and uncomplicated start.
 
-# In this example we assume you have set in config.py
-
-# ncep_dir = '/path/to/weather_data/'
-
-# where the files have format e.g.
-
-# 'ncep_dir/{year}{month:0>2}/tmp2m.*.grb2'
-
+# Register a logger
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 import atlite
 
+# Setup atlite to use the local directory for
+# storing the cutout reanalysis data
+atlite.config.cutout_dir = "./"
+
+# Define the cutout; this will not yet trigger any major operations
 cutout = atlite.Cutout(name="europe-2011-01",
-                       module="ncep",
+                       module="era5",
                        xs=slice(-12.18798349, 41.56244222),
                        ys=slice(71.65648314, 33.56459975),
                        years=slice(2011, 2011),
                        months=slice(1,1))
 
-#this is where all the work happens - it took 105 minutes on FIAS'
-#beast resi, with 16 cores; the resulting cutout takes 57 GB
+# This is where all the work happens
 cutout.prepare()
