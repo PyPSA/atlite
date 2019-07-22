a4951f3a15223409dc41f151454c4667e30332b0
euronion <42553970+euronion@users.noreply.github.com>
2019-07-22 17:11:38 +0200

    Provide oedb adapter for downlading wind turbine configs.

--- atlite/wind.py
+++ atlite/wind.py
@@ -21,6 +21,17 @@ Light-weight version of Aarhus RE Atlas for converting weather data to power sys
 """
 import xarray as xr
 import numpy as np
+import requests
+import pandas as pd
+import json
+import yaml
+import os
+
+from . import config
+from . import utils
+
+import logging
+logger = logging.getLogger(__name__)
 
 def extrapolate_wind_speed(ds, to_height, from_height=None):
     """Extrapolate the wind speed from a given height above ground to another.
@@ -90,3 +101,97 @@ def extrapolate_wind_speed(ds, to_height, from_height=None):
                           "units" : "m s**-1"})
 
     return wnd_spd.rename(to_name)
+
+def download_turbineconf(requested_turbine=None):
+    """Download a windturbine configuration from the OEDB database.
+    
+    Download the configuration of a windturbine model from the OEDB database
+    into the local 'windturbine_dir'.
+    The OEDB database can be viewed here:
+    https://openenergy-platform.org/dataedit/view/supply/turbine_library
+    (2019-07-22)
+    Only one turbine configuration is downloaded at a time, if the 
+    search parameters yield an ambigious result, not data is downloaded.
+    
+    Parameters
+    ----------
+    requested_turbine : dict
+        Search parameters, either provide the turbine model id (takes priority)
+        or a manufacturer and turbine name, e.g. the following are identical:
+        {'id':10}, {'id':10, 'manufacturer':'Unknown', name:'E-53/800'},
+        {'manufacturer':'Enercon', name:'E-53/800'}
+        
+    Returns
+    -------
+    turbineconf : dict
+        The turbine configuration downloaded and stored is also returned as a dict.
+        Has the same format as returned by 'atlite.ressource.get_turbineconf(name)'.
+    """
+    
+    # Get the turbine list
+    oedb_url = 'https://openenergy-platform.org/api/v0/schema/supply/tables/turbine_library/rows'
+    result = requests.get(oedb_url)
+
+    # Convert JSON to dataframe for easier filtering
+    # Only consider turbines with power curves available
+    df = pd.DataFrame.from_dict(result.json())
+    df = df[df.has_power_curve]
+
+    if requested_turbine.get('id'):
+        logger.info("Searching turbine power curve in OEDB database using id "
+                    "'{id}'".format(id=requested_turbine['id'])
+                   )
+        ds = df[df.id == requested_turbine['id']]
+        
+    else:
+        if not isinstance(requested_turbine.get('name'),str) or
+           not isinstance(requested_turbine.get('manufacturer'),str):
+            
+            logger.error("'name' and 'manufacturer' must be provided. "
+                         "Alternatively provide a turbine id from OEDB.")
+            return None
+        logger.info("Searching turbine power curve in OEDB database using "
+                    "manufacturer '{m}' and name '{n}'."
+                    "".format(m=requested_turbine['manufacturer'],
+                              n=requested_turbine['name']))
+            
+        ds = df[df.name == requested_turbine['name']]
+        ds = ds[ds.manufacturer == requested_turbine['manufacturer']]
+
+    if len(ds) < 1 :
+        logger.info("No turbine found for {rt}.".format(rt=requested_turbine))
+    elif len(ds) > 2 :
+        logger.info("Provided information corresponds to more than one turbine. "
+                    "Provide id to unambigious lookup. {rt}."
+                    "".format(rt=requested_turbine))
+    elif len(ds) == 1:
+        # Convert to series for simpliticty
+        ds = ds.iloc[0]
+
+    # convert power from kW to MW
+    power = np.array(json.loads(ds.power_curve_values)) / 1e3
+
+    turbineconf = {
+        "name": ds['name'].strip(),
+        "manufacturer": ds.manufacturer.strip(),
+        "source": "Original: {origin}. "
+                  "Via OEDB {secondary}".format(origin=ds.source, secondary=oedb_url),
+        "HUB_HEIGHT": ds.hub_height.strip(),
+        "V": json.loads(ds.power_curve_wind_speeds),
+        "POW": power.tolist(),
+    }
+
+    filename = "{m}_{n}.yaml".format(m=turbineconf['manufacturer'],
+                                     n=turbineconf['name'])
+    filename = filename.replace("/","_")
+    filename = filename.replace(" ","_")
+    filepath = utils.construct_filepath(os.path.join(config.windturbine_dir, filename))
+
+    with open(filepath, 'w') as turbine_file:
+        yaml.dump(turbineconf, turbine_file)
+        
+    logger.info("Turbine configuration downloaded to '{fp}'.".format(fp=filepath))
+    if ";" in turbineconf['HUB_HEIGHT']:
+        logger.info("Multiple HUB_HEIGHTS in dataset. Manual clean-up required.")
+
+    return turbineconf
