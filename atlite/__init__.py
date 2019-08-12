073c1ac64dc08af0960ca758fc4c3a6f89dd33e0
euronion <42553970+euronion@users.noreply.github.com>
2019-08-12 22:21:42 +0200

    __init__.py: Include new package docstring.

--- atlite/__init__.py
+++ atlite/__init__.py
@@ -4,6 +4,15 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+"""
+Atlite helps you to convert weather data into energy systems model data.
+
+Atlite is a free software, xarray-based Python library for converting weather data 
+(like wind speeds) into energy systems data. It is designed to by lightweight and 
+work with big weather datasets while keeping the resource requirements especially 
+on CPU and RAM resources low.
+"""
+
 from .cutout import Cutout
 from .gis import compute_indicatormatrix, regrid
 
