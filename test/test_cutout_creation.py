65e5ed8ee5fa8bf1817145223da0c59120e20a54
Fabian <fab.hof@gmx.de>
2020-05-11 13:22:52 +0200

    add test for loading and preparation

--- test/test_cutout_creation.py
+++ test/test_cutout_creation.py
@@ -16,52 +16,62 @@ import numpy as np
 
 
 time='2013-01-01'
-x0 = -4
-y0 = 56
+x0 = -4.
+y0 = 56.
 x1 = 1.5
-y1 = 61
+y1 = 61.
 
 
-ref = Cutout(path="era5_test", module="era5", bounds=(x0, y0, x1, y1), time=time)
+ref = Cutout(name="creation_ref", module="era5", bounds=(x0, y0, x1, y1), time=time)
+
+new_feature_tests = False
 
 def test_odd_bounds_coords():
-    cutout = Cutout(path="era5_odd_bounds", module="era5", time=time,
-                    bounds=(x0+0.1, y0-0.02, x1+0.03, y1+0.13))
-    assert_equal(cutout.data.coords.to_dataset(), ref.data.coords.to_dataset())
+    cutout = Cutout(path="odd_bounds", module="era5", time=time,
+                    bounds=(x0-0.1, y0-0.02, x1+0.03, y1+0.13))
+    assert_equal(cutout.coords.to_dataset(), ref.coords.to_dataset())
 
 
 def test_xy_coords():
-    cutout = Cutout(path="era5_odd_bounds", module="era5", time=time,
+    cutout = Cutout(path="xy", module="era5", time=time,
                     x=slice(x0, x1), y = slice(y0, y1))
-    assert_equal(cutout.data.coords.to_dataset(), ref.data.coords.to_dataset())
+    assert_equal(cutout.coords.to_dataset(), ref.coords.to_dataset())
 
 
 def test_xy_reversed_coords():
-    cutout = Cutout(path="era5_odd_bounds", module="era5", time=time,
+    cutout = Cutout(path="xy_r", module="era5", time=time,
                     x=slice(x1, x0), y = slice(y1, y0))
-    assert_equal(cutout.data.coords.to_dataset(), ref.data.coords.to_dataset())
+    assert_equal(cutout.coords.to_dataset(), ref.coords.to_dataset())
 
 
-# I think this would be nice to have
-
-# def test_xy_tuple_coords():
-#     cutout = Cutout(path="era5_odd_bounds", module="era5", time=time,
-#                     x=(-14.1, 1.512), y =(50.94, 61.123))
-#     assert_equal(cutout.data.coords.to_dataset(), ref.data.coords.to_dataset())
+if new_feature_tests:
+    def test_xy_tuple_coords():
+        cutout = Cutout(path="xy_tuple", module="era5", time=time,
+                        x=(-14.1, 1.512), y =(50.94, 61.123))
+        assert_equal(cutout.coords.to_dataset(), ref.coords.to_dataset())
 
 
 def test_time_sclice_coords():
-    cutout = Cutout(path="era5_odd_bounds", module="era5",
-                    time=('2013-01-01 00:00', '2013-01-01 23:00'),
+    cutout = Cutout(path="time_slice", module="era5",
+                    time=slice('2013-01-01', '2013-01-01'),
                     x=slice(x0, x1), y = slice(y0, y1))
-    assert_equal(cutout.data.coords.to_dataset(), ref.data.coords.to_dataset())
+    assert_equal(cutout.coords.to_dataset(), ref.coords.to_dataset())
 
 
-def test_time_period_coords():
-    cutout = Cutout(path="era5_odd_bounds", module="era5",
-                    time=pd.Period('2013-01-01'),
-                    x=slice(x0, x1), y = slice(y0, y1))
-    assert_equal(cutout.data.coords.to_dataset(), ref.data.coords.to_dataset())
+if new_feature_tests:
+    def test_time_tuple_coords():
+        cutout = Cutout(path="time_tuple", module="era5",
+                        time=('2013-01-01', '2013-01-01'),
+                        x=slice(x0, x1), y = slice(y0, y1))
+        assert_equal(cutout.coords.to_dataset(), ref.coords.to_dataset())
+
+
+if new_feature_tests:
+    def test_time_period_coords():
+        cutout = Cutout(path="time_period", module="era5",
+                        time=pd.Period('2013-01-01'),
+                        x=slice(x0, x1), y = slice(y0, y1))
+        assert_equal(cutout.coords.to_dataset(), ref.coords.to_dataset())
 
 
 def test_module_assignment():
--- test/test_cutout_preparation.py
+++ test/test_cutout_preparation.py
@@ -24,53 +24,43 @@ x0 = -4
 y0 = 56
 x1 = 1.5
 y1 = 61
+path="era5_test"
+
+if os.path.exists(path + '.nc'):
+    os.remove(path + '.nc')
+
+ref = Cutout(path=path, module="era5", bounds=(x0, y0, x1, y1), time=time)
+ref.prepare(overwrite=True)
+
+# Backwards compatibility with name and cutout_dir
+def test_old_style_loading_args():
+    cutout = Cutout(name=path, cutout_dir='.')
+    assert_equal(cutout.data.coords.to_dataset(), ref.data.coords.to_dataset())
+
+    cutout = Cutout(name=path)
+    assert_equal(cutout.data.coords.to_dataset(), ref.data.coords.to_dataset())
 
 
-ref = Cutout(path="era5_test", module="era5", bounds=(x0, y0, x1, y1), time=time)
-
-# assert_allclose(era5.data.wnd100m, era5_odd_bounds.data.wnd100m,
-#                 atol=1e-5, rtol=1e-5)
-
-
-# period = pd.Period(time)
-# influx = atlite.datasets.era5.get_data(era5.coords, period, 'influx', tmpdir='era5_temps')
-# influx = influx.compute()
+def test_odd_bounds():
+    cutout = Cutout(path = path + '_odd_bounds', module="era5", time=time,
+                    bounds=(x0-0.1, y0-0.02, x1+0.03, y1+0.13))
+    cutout.prepare(overwrite=True)
+    assert_allclose(cutout.data.wnd100m, ref.data.wnd100m,
+                    atol=1e-5, rtol=1e-5)
 
 
-# period = pd.Period(time)
-# influx = atlite.datasets.era5.get_data(era5.coords, period, 'influx', tmpdir='era5_temps')
-# influx = influx.compute()
-
-# temperature = atlite.datasets.era5.get_data(era5.coords, period, 'temperature', tmpdir='era5_temps')
-# temperature.compute()
-# %%
-
-# if os.path.exists('sarah_test.nc'):
-#     os.remove('sarah_test.nc')
-# creation_params = dict(time="2013-12-03",
-#                        x = [-14, 1.5], y = [50, 61],
-#                        sarah_dir='/home/vres/climate-data/sarah_v2')
-# sarah = atlite.Cutout(path="sarah_test", module="sarah", **creation_params)
-
-# sarah.prepare()
-
-# data = atlite.datasets.sarah.get_data_sarah(sarah.coords, pd.Period('2013'),
-#                                             **creation_params).compute()
-# data.influx_direct.isnull().sum().compute()
+def test_compare_with_get_data_era5():
+    period = pd.Period(time)
+    influx = atlite.datasets.era5.get_data(ref.coords, period, 'influx', tmpdir='.')
+    influx = influx.compute()
+    assert_allclose(influx.influx_toa, ref.data.influx_toa, atol=1e-5, rtol=1e-5)
 
 
+def test_get_era5_data_in_sarah_module():
+    period = pd.Period(time)
+    influx = atlite.datasets.sarah.get_data_era5(ref.coords, period, 'influx',
+                                                 tmpdir='.', x=None, y=None, sarah_dir=None)
+    influx = influx.compute()
+    assert_allclose(influx.influx_toa, ref.data.influx_toa, atol=1e-5, rtol=1e-5)
 
-# sarah.data.influx_direct.sel(time=sarah.data.time[12]).plot()
-
-
-# import os
-# import atlite
-
-# cutout = atlite.Cutout(path="../cutouts/test-cutout-sarah2_europe.nc",
-#         bounds=(-12, 41.8, 33. 64.8),
-#         module="sarah",
-#         time=slice("2013-01-01", "2013-12-31"),
-#         sarah_dir='/home/vres/climate-data/sarah_v2')
-
-# cutout.prepare()
 
