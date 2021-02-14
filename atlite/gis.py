4be957c6cfb3630ba440424df9cc58af7eaaf9c5
Fabian <fab.hof@gmx.de>
2021-02-14 12:56:16 +0100

    gis: add availability matrix calculator

--- atlite/gis.py
+++ atlite/gis.py
@@ -14,12 +14,19 @@ import xarray as xr
 import scipy as sp
 import scipy.sparse
 from collections import OrderedDict
+from pathlib import Path
 from warnings import warn
 from pyproj import CRS, Transformer
 import geopandas as gpd
 from shapely.ops import transform
 import rasterio as rio
 import rasterio.warp
+from rasterio.warp import reproject, transform_bounds
+from rasterio.mask import mask
+from rasterio.plot import show
+from rasterio.features import geometry_mask
+from scipy.ndimage.morphology import binary_dilation as dilation
+from numpy import isin, empty, where
 from shapely.strtree import STRtree
 
 import logging
@@ -101,7 +108,7 @@ reproject.__doc__ = reproject_shapes.__doc__
 
 def compute_indicatormatrix(orig, dest, orig_crs=4326, dest_crs=4326):
     """
-    Compute the indicatormatrix
+    Compute the indicatormatrix.
 
     The indicatormatrix I[i,j] is a sparse representation of the ratio
     of the area in orig[j] lying in dest[i], where orig and dest are
@@ -137,6 +144,171 @@ def compute_indicatormatrix(orig, dest, orig_crs=4326, dest_crs=4326):
                 indicator[i, j] = area / o.area
 
     return indicator
+
+
+class ExclusionContainer:
+    """Container for exclusion objects and meta data."""
+
+    def __init__(self, crs=3035, res=100):
+        """Initialize a container for excluded areas.
+
+        Parameters
+        ----------
+        crs : rasterio.CRS/proj.CRS/EPSG, optional
+            Base crs of the raster collection. All rasters and geometries
+            diverging from this crs will be converted to it.
+            The default is 3035.
+        res : float, optional
+            Resolution of the base raster. All diverging rasters will be
+            resmapled using the gdal Resampling method 'nearest'.
+            The default is 100.
+        """
+        self.rasters = []
+        self.geometries = []
+        self.crs = crs
+        self.res = res
+
+    def add_raster(self, raster, codes=1, dilation=0, invert=False, **kwargs):
+        """
+        Register a raster to the Excluder.
+
+        Parameters
+        ----------
+        raster : str/rasterio.DatasetReader
+            Path to raster or raster which to exclude.
+        codes : int/list, optional
+            Codes in the raster which to exclude/include. The default is 1.
+        dilation : int, optional
+            Dilation of the excluded areas in cell. Use this to create a buffer
+            around the excluded/included area. The default is 0.
+        invert : bool, optional
+            Whether to exclude (False) or include (True) the specified areas
+            of the raster. The default is False.
+        crs : rasterio.CRS/EPSG
+            CRS of the raster. Specify this if the raster has crs data missing.
+        **kwargs
+        """
+        d = dict(raster=raster, codes=codes, dilation=dilation,
+                 invert=invert, **kwargs)
+        self.rasters.append(d)
+
+    def add_geometry(self, geometry, buffer=0, invert=False, **kwargs):
+        """
+        Register a collection of geometries to the Excluder.
+
+        Parameters
+        ----------
+        geometry : str/path/geopandas.GeoDataFrame
+            Path to geometries or geometries which to exclude.
+        buffer : float, optional
+            Buffer around the geometries. The default is 0.
+        invert : bool, optional
+            Whether to exclude (False) or include (True) the specified areas
+            of the geometries. The default is False.
+        **kwargs
+        """
+        d = dict(buffer=buffer, invert=invert, **kwargs)
+        self.geometries.append(d)
+
+
+    def initialize(self):
+        """Open rasters and load geometries."""
+        for d in self.rasters:
+            raster = d['raster']
+            if isinstance(raster, (str, Path)):
+                raster = rio.open(raster)
+            else:
+                assert isinstance(raster, rio.DatasetReader)
+            if 'crs' in d:
+                raster._crs = d['crs']
+            d['raster'] = raster
+
+        for d in self.geometries:
+            geometry = d['geometry']
+            if isinstance(geometry, (str, Path)):
+                geometry = gpd.read_file(geometry)
+            assert isinstance(geometry, gpd.GeoDataFrame)
+            geometry.to_crs(self.crs)
+            d['geometry'] = geometry.geometry
+
+    def __repr__(self):
+        return (f"Exclusion Container"
+                f"\n registered rasters: {len(self.rasters)} "
+                f"\n registered geometry collections: {len(self.geometries)}"
+                f"\n CRS: {self.crs} - Resolution: {self.res}")
+
+
+def padded_transform_and_shape(bounds, res):
+    left, bottom = [(b // res)* res for b in bounds[:2]]
+    right, top = [(b // res + 1) * res for b in bounds[2:]]
+    shape = int((top - bottom) // res), int((right - left) / res)
+    return rio.Affine(res, 0, left, 0, -res, top), shape
+
+
+def projected_mask(raster, geom, transform=None, shape=None, crs=None, **kwargs):
+    """Load a mask and optionally project it to target resolution and shape."""
+    kwargs.setdefault('indexes', 1)
+    masked, transform_ = mask(raster, geom, crop=True, **kwargs)
+
+    if transform is None or (transform_ == transform):
+        return masked, transform_
+
+    assert shape is not None and crs is not None
+    return rio.warp.reproject(masked, empty(shape), src_crs=raster.crs, dst_crs=crs,
+                     src_transform=transform_, dst_transform=transform)
+
+
+def pad_extent(values, src_transform, dst_transform, src_crs, dst_crs):
+    """Ensure the array is large enough to not be treated as nodata."""
+    left, top, right, bottom = *(src_transform*(0,0)), *(src_transform*(1,1))
+    covered = transform_bounds(src_crs, dst_crs, left, bottom, right, top)
+    covered_res = min(covered[2] - covered[0], covered[3] - covered[1])
+    pad = int(dst_transform[0] // covered_res * 1.1)
+    return rio.pad(values, src_transform, pad, 'constant', constant_values=0)
+
+
+def shape_availability(geometry, excluder):
+    exclusions = []
+    crs = excluder.crs
+
+    bounds = rio.features.bounds(geometry)
+    transform, shape = padded_transform_and_shape(bounds, res=excluder.res)
+    masked = geometry_mask(geometry, shape, transform).astype(int)
+    exclusions.append(masked)
+
+    raster = None
+    for d in excluder.rasters:
+        # allow reusing preloaded raster with different config
+        if raster != d['raster']:
+            raster = d['raster']
+            nodata = d.get('nodata', 255)
+            masked, transform = projected_mask(d['raster'], geometry, transform,
+                                               shape, crs, nodata=nodata)
+        masked_ = masked
+        # ...
+        exclusions.append(masked_)
+    return (sum(exclusions) == 0).astype(float), transform
+
+
+def compute_availabilitymatrix(cutout, shapes, excluder):
+    availability = []
+    names = shapes.get('name', shapes.index)
+    shapes = shapes.geometry if isinstance(shapes, gpd.GeoDataFrame) else shapes
+
+    for i in shapes.index:
+        masked, transform = shape_availability(shapes[[i]], excluder)
+        masked, transform = pad_extent(masked, transform, cutout.transform,
+                                       excluder.crs, cutout.crs)
+
+        _ = rio.warp.reproject(masked, empty(cutout.shape), resampling=5,
+                               src_transform=transform,
+                               dst_transform=cutout.transform,
+                               src_crs=excluder.crs, dst_crs=cutout.crs,)[0]
+        availability.append(_)
+    coords=[('shapes', names), ('y', cutout.data.y), ('x', cutout.data.x),]
+    return xr.DataArray(np.stack(availability), coords=coords)
+
+
 
 
 def maybe_swap_spatial_dims(ds, namex='x', namey='y'):
