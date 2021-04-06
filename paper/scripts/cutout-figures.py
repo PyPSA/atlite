2caa26ccd2799e6e625bffb3a4a70fed64520784
Fabian <fab.hof@gmx.de>
2021-04-06 16:15:23 +0200

    add code for cutout figure

--- paper/scripts/cutout-figures.py
+++ paper/scripts/cutout-figures.py
@@ -10,28 +10,118 @@ import atlite
 import matplotlib.pyplot as plt
 import geopandas as gpd
 import cartopy.crs as ccrs
+import numpy as np
+import cartopy
+import xarray as xr
 from cartopy.crs import PlateCarree as plate
-import cartopy.io.shapereader as shpreader
-from pathlib import Path 
+from pathlib import Path
+from matplotlib.gridspec import GridSpec
 
-
-plt.rc('figure', figsize=(10, 6))
-
+plt.rc('text', usetex=True)
+plt.rc('font', family='sans-serif', size=13)
 figpath = Path('../figures/')
+figpath.mkdir(exist_ok=True)
+
+# from https://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/administrative-units-statistical-units/nuts#nuts21
+nutsregions = gpd.read_file('NUTS_RG_60M_2021_4326.geojson')
+regions = nutsregions.query('CNTR_CODE == "DE" and LEVL_CODE == 2')
+de = regions.unary_union
 
 
-        
+centroid = regions.total_bounds[[0, 2]].mean(), regions.total_bounds[[1,3]].mean()
+projection = ccrs.Orthographic(*centroid)
 
 
-cutout = atlite.Cutout('cutout', x=slice(10, 30), y=slice(40,50), time='2013', module='era5')
+cutout = atlite.Cutout('cutout', bounds=de.buffer(1).bounds, time='2013-01',
+                       module='era5', dx=.5, dy=.5)
+# cutout.prepare(features="wind")
+cutout_bound = gpd.GeoSeries(cutout.grid.unary_union)
 
 
-# fig, ax = plt.subplots()
+#%% all steps in one
+fig = plt.figure(figsize=(11, 9))
+gs = GridSpec(6, 5, figure=fig)
+title_params = dict(loc='left', multialignment='left')
+
+# cutout creation plot
+color = 'teal'
+ax = fig.add_subplot(gs[:3, 0:3], projection=projection)
+cutout.grid.plot(ax=ax, facecolor=color, edgecolor='white', linewidth=1.5, alpha=0.3, transform=plate())
+cutout.grid[['x', 'y']].plot.scatter(x='x', y='y', ax=ax, transform=plate(),
+                                     color=color, s=1, alpha=1)
+cutout_bound.plot(ax=ax, edgecolor='orange', facecolor='None', transform=plate())
+ax.set_extent(np.array(de.buffer(2).bounds)[[0,2,1,3]], crs=plate())
+ax.add_feature(cartopy.feature.BORDERS.with_scale('10m'), edgecolor='gray')
+ax.add_feature(cartopy.feature.COASTLINE.with_scale('10m'), edgecolor='gray')
+ax.axis('off')
+ax.set_title(r"1. Create Cutout", **title_params)
+
+
+# Prepare cutout
+df = cutout.data.mean(['x', 'y']).to_dataframe()
+
+ax = fig.add_subplot(gs[0, 3:])
+df.wnd100m.plot(ax=ax, color='teal')
+ax.set_title("2. Prepare Cutout", **title_params)
+ax.spines[['top', 'right']].set_visible(False)
+ax.set_ylabel('wnd100m [m/s]')
+ax.set_xlabel('')
+ax.set_xticklabels([])
+
+ax = fig.add_subplot(gs[1, 3:])
+df.roughness.plot(ax=ax, color='orange')
+ax.spines[['top', 'right']].set_visible(False)
+ax.set_ylabel('roughness [m]')
+ax.set_xlabel('')
+
+
+ax = fig.add_subplot(gs[2, 3:])
+ax.text(x=.5, y=.8, s=r'\noindent .\\ . \\ .', ha='center', fontsize=30)
+ax.axis('off')
+
+
+# Convert cutout
+turbine = atlite.windturbines.Vestas_V112_3MW
+cap_per_sqkm = 1.7
+layout = cutout.grid.set_index(['x', 'y']).to_crs(3025).area / 1e6 * cap_per_sqkm
+layout = xr.DataArray(layout.unstack())
+ds, capacity = cutout.wind(turbine, shapes=regions, layout=layout,
+                           return_capacity=True, per_unit=True)
+
+
+ax = fig.add_subplot(gs[3:, :3])
+ax.set_title("3. Convert to potentials and timeseries per region", **title_params)
+regions.plot(ax=ax, column=capacity.to_series()/1e3, legend=True, cmap='Greens',
+              legend_kwds=dict(label='Potential Capcity [GW]',
+                               location='right', shrink=0.8)
+             )
+# cutout.grid.plot(ax=ax, facecolor='None', edgecolor='grey', ls=':')
+ax.axis('off')
+
+color = 'steelblue'
+ax = fig.add_subplot(gs[3, 3:])
+ds.isel(dim_0=0).to_series().plot(ax=ax, color=color)
+ax.set_title('Region 1', fontsize=11)
+ax.spines[['top', 'right']].set_visible(False)
+ax.set_ylabel('Power p.u.')
+ax.set_xlabel('')
+ax.set_xticklabels([])
+
+ax = fig.add_subplot(gs[4, 3:])
+ds.isel(dim_0=1).to_series().plot(ax=ax, color=color)
+ax.set_title('Region 2', fontsize=11)
+ax.spines[['top', 'right']].set_visible(False)
+ax.set_ylabel('Power p.u.')
+ax.set_xlabel('')
+
+ax = fig.add_subplot(gs[5, 3:])
+ax.text(x=.5, y=.8, s=r'\noindent .\\ . \\ .', ha='center', fontsize=30)
+ax.axis('off')
+
+
+fig.tight_layout()
 
 
 
-# shpfilename = shpreader.natural_earth(resolution='10m', category='cultural',
-                                      # name='admin_0_countries')
-# reader = shpreader.Reader(shpfilename)
-# UkIr = gpd.GeoSeries({r.attributes['NAME_EN']: r.geometry for r in reader.records()},
-                     # crs={'init': 'epsg:4326'}).reindex(['United Kingdom', 'Ireland'])
+
+
