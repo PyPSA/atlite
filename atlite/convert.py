8ba1a62b606bdb7bf30c641917ffbc933b1cb22a
euronion <42553970+euronion@users.noreply.github.com>
2021-06-22 13:55:08 +0200

    Pass dask keywords properly.

--- atlite/convert.py
+++ atlite/convert.py
@@ -120,9 +120,9 @@ def convert_and_aggregate(
                 "given for `per_unit` or `return_capacity`"
             )
         if capacity_factor:
-            return maybe_progressbar(da.mean("time"), show_progress)
+            return maybe_progressbar(da.mean("time"), show_progress, **dask_kwargs)
         else:
-            return maybe_progressbar(da.sum("time"), show_progress)
+            return maybe_progressbar(da.sum("time"), show_progress, **dask_kwargs)
 
     if shapes is not None:
         geoseries_like = (pd.Series, gpd.GeoDataFrame, gpd.GeoSeries)
