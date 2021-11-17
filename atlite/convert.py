967ba901c9ec6317cf78cf9656ed0ce7ef499d9b
euronion <42553970+euronion@users.noreply.github.com>
2021-11-17 10:57:44 +0100

    Adjust conversion method to take into account solar position dependend efficiency.

--- atlite/convert.py
+++ atlite/convert.py
@@ -504,7 +504,7 @@ def pv(cutout, panel, orientation, clearsky_model=None, **params):
 
 def convert_csp(ds, technology, installation):
 
-    solar_position = SolarPosition(da)
+    solar_position = SolarPosition(ds)
 
     if technology == "parabolic trough":
         irradiation = ds["influx_direct"]
@@ -513,7 +513,17 @@ def convert_csp(ds, technology, installation):
     else:
         raise ValueError(f'Unknown CSP technology option "{technology}".')
 
-    return installation * irradiation
+    # Determine solar_position dependend efficiency for each grid cell and time step
+    efficiency = installation.interp(
+        altitude=solar_position['altitude'],
+        azimuth=solar_position['azimuth'],
+        # faster than lin. interpolation; sufficient for high res efficiency map 'installation'
+        method='nearest',
+        # Fill values outside efficiencies specified with alt/az.
+        kwargs={'fill_value':0.}
+    )
+
+    return efficiency * irradiation
 
 
 def csp(cutout, technology, installation, **params):
