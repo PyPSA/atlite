761e63d8edf4bcc6b4db273ae22b92f3c7002105
euronion <42553970+euronion@users.noreply.github.com>
2021-11-22 22:17:07 +0100

    Update copyright years.

--- atlite/datasets/sarah.py
+++ atlite/datasets/sarah.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# SPDX-FileCopyrightText: 2016-2019 The Atlite Authors
+# SPDX-FileCopyrightText: 2016-2021 The Atlite Authors
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
