6bf14a10479a829b28e8c82aebd8cd61ec9c258d
davide-f <fioritidavidesubs@gmail.com>
2022-06-07 10:36:49 +0200

    Change xarray shift with roll

--- atlite/hydro.py
+++ atlite/hydro.py
@@ -103,6 +103,6 @@ def shift_and_aggregate_runoff_for_plants(
         nhours = (distances * (flowspeed * 3.6) + 0.5).astype(int)
 
         for b in ppl.upstream:
-            upstream_inflow_b = runoff.sel(hid=b).shift(time=nhours.at[b], fill_value=0)
+            inflow_plant += runoff.sel(hid=b).roll(time=nhours.at[b])
 
     return inflow
