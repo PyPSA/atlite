feat(wind): Bias correct wind speeds based on scaling to a known average
The ERA5 long run average is determined during cutout preparation by averaging
the monthly mean wind speeds from 1980 to the last year and made available as
a dataset variable wnd100m_lra.

Example usage:

cutout.wind(
    atlite.windturbines.Enercon_E101_3000kW,
    real_long_run_average_windspeed="gwa3_250_windspeed_100m.tif",
)

atlite/convert.py
@@ -478,19 +478,28 @@ def solar_thermal(
 
 
 # wind
-def convert_wind(ds, turbine):
+def convert_wind(
+    ds: xr.Dataset, turbine, windspeed_bias_correction=None, from_height=None
+):
     """
     Convert wind speeds for turbine to wind energy generation.
     """
     V, POW, hub_height, P = itemgetter("V", "POW", "hub_height", "P")(turbine)
 
-    wnd_hub = windm.extrapolate_wind_speed(ds, to_height=hub_height)
+    if windspeed_bias_correction is not None:
+        ds = ds.assign(
+            {f"wnd{from_height}m": ds[f"wnd{from_height}m"] * windspeed_bias_correction}
+        )
 
-    def _interpolate(da):
+    wnd_hub = windm.extrapolate_wind_speed(
+        ds, to_height=hub_height, from_height=from_height
+    )
+
+    def apply_power_curve(da):
         return np.interp(da, V, POW / P)
 
     da = xr.apply_ufunc(
-        _interpolate,
+        apply_power_curve,
         wnd_hub,
         input_core_dims=[[]],
         output_core_dims=[[]],
@@ -503,7 +512,15 @@ def convert_wind(ds, turbine):
     return da
 
 
-def wind(cutout, turbine, smooth=False, add_cutout_windspeed=False, **params):
+def wind(
+    cutout,
+    turbine,
+    smooth=False,
+    add_cutout_windspeed=False,
+    real_long_run_average_windspeed=None,
+    real_long_run_average_height=100,
+    **params,
+):
     """
     Generate wind generation time-series.
 
@@ -529,6 +546,10 @@ def wind(cutout, turbine, smooth=False, add_cutout_windspeed=False, **params):
         output at the highest wind speed in the power curve. If False, a warning will be
         raised if the power curve does not have a cut-out wind speed. The default is
         False.
+    real_long_run_average_speed : Path or rasterio dataset or None
+        Raster dataset with wind speeds to bias correct long run average wind speeds
+    real_long_run_average_height : int = 100
+        Height in meters of provided real_long_average_speed dataset
 
     Note
     ----
@@ -549,8 +570,24 @@ def wind(cutout, turbine, smooth=False, add_cutout_windspeed=False, **params):
     if smooth:
         turbine = windturbine_smooth(turbine, params=smooth)
 
+    windspeed_bias_correction = None
+    from_height = None
+
+    if real_long_run_average_windspeed is not None:
+        windspeed_bias_correction = windm.calculate_windspeed_bias_correction(
+            cutout.data,
+            real_long_run_average_windspeed,
+            real_long_run_average_height,
+            crs=cutout.crs,
+        )
+        from_height = real_long_run_average_height
+
     return cutout.convert_and_aggregate(
-        convert_func=convert_wind, turbine=turbine, **params
+        convert_func=convert_wind,
+        turbine=turbine,
+        windspeed_bias_correction=windspeed_bias_correction,
+        from_height=from_height,
+        **params,
     )
 
 

atlite/wind.py
@@ -7,8 +7,13 @@ Functions for use in conjunction with wind data generation.
 
 import logging
 import re
+from pathlib import Path
 
 import numpy as np
+import rasterio as rio
+import xarray as xr
+
+from .gis import _as_transform
 
 logger = logging.getLogger(__name__)
 
@@ -51,14 +56,11 @@ def extrapolate_wind_speed(ds, to_height, from_height=None):
     Retrieved 2019-02-15.
 
     """
-    # Fast lane
-    to_name = f"wnd{int(to_height):0d}m"
-    if to_name in ds:
-        return ds[to_name]
-
     if from_height is None:
         # Determine closest height to to_name
-        heights = np.asarray([int(s[3:-1]) for s in ds if re.match(r"wnd\d+m", s)])
+        heights = np.asarray(
+            [int(m.group(1)) for s in ds if (m := re.match(r"wnd(\d+)m", s))]
+        )
 
         if len(heights) == 0:
             raise AssertionError("Wind speed is not in dataset")
@@ -67,17 +69,46 @@ def extrapolate_wind_speed(ds, to_height, from_height=None):
 
     from_name = f"wnd{int(from_height):0d}m"
 
+    # Fast lane
+    if from_height == to_height:
+        return ds[from_name]
+
     # Wind speed extrapolation
     wnd_spd = ds[from_name] * (
         np.log(to_height / ds["roughness"]) / np.log(from_height / ds["roughness"])
     )
 
-    wnd_spd.attrs.update(
+    return wnd_spd.assign_attrs(
         {
             "long name": f"extrapolated {to_height} m wind speed using logarithmic "
             f"method with roughness and {from_height} m wind speed",
             "units": "m s**-1",
         }
-    )
+    ).rename(f"wnd{to_height}m")
 
-    return wnd_spd.rename(to_name)
+
+def calculate_windspeed_bias_correction(ds, real_lra, lra_height, crs):
+    data_lra = ds[f"wnd{lra_height}m_lra"]
+
+    if isinstance(real_lra, (str, Path)):
+        real_lra = rio.open(real_lra)
+
+    if isinstance(real_lra, rio.DatasetReader):
+        real_lra = rio.band(real_lra, 1)
+
+    if isinstance(real_lra, rio.Band):
+        real_lra, transform = rio.warp.reproject(
+            real_lra,
+            np.empty(data_lra.shape),
+            dst_crs=crs,
+            dst_transform=_as_transform(
+                x=data_lra.indexes["x"], y=data_lra.indexes["y"]
+            ),
+            resampling=rio.enums.Resampling.average,
+        )
+
+        real_lra = xr.DataArray(
+            real_lra, [data_lra.indexes["y"], data_lra.indexes["x"]]
+        )
+
+    return real_lra / data_lra